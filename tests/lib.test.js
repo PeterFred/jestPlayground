3f02cd28f869d1abea276ce98ebfdf060775a103
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-12-11 12:42:35 +1300

    Utilise 'describe' and 'it' to group tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,8 +1,20 @@
 const lib = require("../lib");
-
 //Check matchers at https://jestjs.io/docs/en/using-matchers
 
-test("absolute - should return a pos num if input is pos", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
+//Group related tests inside a describe block
+describe("absolute", () => {
+  it("should return a pos num if input is pos", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+
+  it("should return a pos num if input is neg", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it("should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
